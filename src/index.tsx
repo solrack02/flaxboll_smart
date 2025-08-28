054d57caab889d8704a19d83aeb0047cd8b95fcb
Carlos A. <carlosvsk@gmail.com>
2025-08-28 16:47:16 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -706,6 +706,7 @@ console.log({item});
 
           args,
         }}/>, 
+        
 
         (...args: any) => <Elements.Screen3 pass={{
           pathScreen:"home",
@@ -719,6 +720,33 @@ console.log({item});
 
           arrStyles: [
             `{ color: 'orange' }`
+          ],
+
+          children: [
+            `Meu primeiro App com IA no Flaxboll`
+          ],
+
+          args,
+
+        }}/>],
+
+          functions:[()=>{}],
+
+          args,
+        }}/>, 
+
+        (...args: any) => <Elements.Screen3 pass={{
+          pathScreen:"home",
+
+          styles:[`{ backgroundColor: 'white', flex: 1, padding: 20, justifyContent: 'flex-start' }`],
+
+          screenElements:[(...args:any) => <Elements.Text pass={{
+          arrProps: [
+            '{}'
+          ],
+
+          arrStyles: [
+            `{ color: 'red', fontSize: 50 }`
           ],
 
           children: [
