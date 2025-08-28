fac2a524039b35f7d5e91c0c90aa7a6d3f03ebe9
Carlos A. <carlosvsk@gmail.com>
2025-08-28 13:47:49 -0300

    Initial commit

---
+++ src/tools/base/project/findFlatItem.ts
@@ -0,0 +1,20 @@
+
+export const findFlatItem = obj => {
+  if (typeof obj !== 'object' || obj === null) return null;
+
+  if ('item' in obj) return obj.item;
+
+  for (const key in obj) {
+    if (Array.isArray(obj[key])) {
+      for (const element of obj[key]) {
+        const found = findFlatItem(element);
+        if (found) return found;
+      }
+    } else if (typeof obj[key] === 'object') {
+      const found = findFlatItem(obj[key]);
+      if (found) return found;
+    }
+  }
+
+  return null;
+};
