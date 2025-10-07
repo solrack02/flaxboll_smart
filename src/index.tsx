c3d9578cb85a9ae6d5ea023c555046ee2ab9bc99
Carlos A. <carlosvsk@gmail.com>
2025-10-07 15:35:04 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -25,6 +25,7 @@
 
           const screens = [
             
+        
 
         (...args: any) => <Elements.Screen3 pass={{
           pathScreen:"home2",
@@ -643,6 +644,49 @@ console.log({item});
           }}/>
         ],
             args,
+        }}/>],
+
+          functions:[()=>{}],
+
+          args,
+        }}/>, 
+
+        (...args: any) => <Elements.Screen3 pass={{
+          pathScreen:"home",
+
+          styles:[`{ flex: 1, backgroundColor: 'white', justifyContent: 'flex-start', alignItems: 'center' }`],
+
+          screenElements:[
+        (...args:any) => <Elements.Text pass={{
+          arrProps: [
+            '{}'
+          ],
+
+          arrStyles: [
+            `{ color: 'red', fontSize: 40 }`
+          ],
+
+          children: [
+            `Carlos Alberto`
+          ],
+
+          args,
+
+        }}/>, (...args:any) => <Elements.Text pass={{
+          arrProps: [
+            '{}'
+          ],
+
+          arrStyles: [
+            `{ color: 'red', fontSize: 40 }`
+          ],
+
+          children: [
+            `Carlos Alberto`
+          ],
+
+          args,
+
         }}/>],
 
           functions:[()=>{}],
