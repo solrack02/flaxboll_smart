ba12cbc086d86fb241540b9169ba36a167f30351
Carlos A. <carlosvsk@gmail.com>
2025-10-16 16:33:50 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -109,7 +109,16 @@ fontSize: 14,
             {}
           ],
 
-          pData: [{name: "Juliana", img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"}, {name: "Carlos", img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg"}],
+          pData: [
+  {
+    name: 'Juliana',
+    img: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg',
+  },
+  {
+    name: 'Carlos',
+    img: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg',
+  },
+],
 
           itemElements: [
             
